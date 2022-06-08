0a03d88c562962d742e07ba0be7f7ef4e2bb361b
Julieta <ochoajulieta67@gmail.com>
2022-06-08 18:04:58 -0300

    Agregar archivos de proyecto.

---
+++ ejercicioVectores/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Ingrese el tamaño del vector");
+int tamanio = int.Parse(Console.ReadLine());
+
+int[] vector = new int[tamanio];
+
+Random aleatorio = new Random();
+
+for (int i = 0; i < vector.Length; i++)
+{
+    vector[i] = aleatorio.Next(100);
+}
+
+foreach (var item in vector)
+{
+    Console.Write(item + " ");
+}
+
+Console.WriteLine();
+Console.WriteLine("El vector invertido es:");
+
+for (int i = vector.Length - 1; i >= 0; i--)
+{
+    Console.Write(vector[i]+ " ");
+}
